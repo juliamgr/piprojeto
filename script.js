0a3d4b13498e5825831a0998a4f32471a81b9d94
Júlia Moreira Gregis dos Reis <85226203004@senacrs.edu.br>
2026-08-07 10:12:17 -0300

    mapa lixo

---
+++ script.js
@@ -0,0 +1,10 @@
+tt.setProductInfo("MeuProjeto", "1.0");
+
+const map = tt.map({
+    key: "XqAXdm4kPhyZUo6UYtMp8MnTLCCHPW9x",
+    container: "map",
+    center: [-51.2177, -30.0346],
+    zoom: 13
+});
+
+map.addControl(new tt.NavigationControl());
